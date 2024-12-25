4be0beefb7b1d3532dd1a773d3fc09cf56752a15
Killhub373 <linggodhanudarojad.tkj2.24@binusasmg.sch.id>
2024-12-25 07:59:21 +0700

    master

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Hero from "./components/Hero";
+import Masakan from "./components/Masakan";
+import Footer from "./components/Footer";
+import Product from "./components/Product";
+import Testimonial from "./components/Testimonial"
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
+  return (
+    <div className="flex flex-col min-h-screen justify-between">
+      <div>
+        <Hero />
+        <Masakan />
+        <Product />
+        <Testimonial />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
